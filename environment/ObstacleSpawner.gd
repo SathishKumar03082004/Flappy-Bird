73f24e34fa08dcadc7e9fb408b5cf3a0a5200c8e
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2023-10-06 17:24:47 +0530

    flappy

--- environment/ObstacleSpawner.gd
+++ environment/ObstacleSpawner.gd
@@ -4,10 +4,21 @@ onready var timer=$Timer
 
 var Obstacle=preload("res://environment/Obstacle.tscn")
 
+func _ready():
+	randomize()
+
 func _on_Timer_timeout():
 	spawn_obstacle()
 
 func spawn_obstacle():
 	var obstacle=Obstacle.instance()
 	add_child(obstacle)
-	obstacle.position.y=randi()
+	#get The Random Number Between 150-550
+	obstacle.position.y=randi()%400+150
+
+
+func start():
+	timer.start()
+	
+func stop():
+	timer.stop()
