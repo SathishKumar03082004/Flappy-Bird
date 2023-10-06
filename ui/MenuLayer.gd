66763dd2fcb1115aed136095f571563ecd33e20e
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2023-10-06 19:25:20 +0530

    flappy

--- ui/MenuLayer.gd
+++ ui/MenuLayer.gd
@@ -2,9 +2,13 @@ extends CanvasLayer
 
 signal start_game
 
+onready var start_message=$StartMenu/StartMessage
+onready var tween=$Tween
+
 var game_started=false
 
 func _input(event):
 	if event.is_action_pressed("flap") && !game_started:
 		emit_signal("start_game")
+		tween.interpolate_property(start_message,"modulate:a",1,0,0.5)
 		game_started=true
