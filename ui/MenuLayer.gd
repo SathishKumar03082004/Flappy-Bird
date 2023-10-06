bb4853b4bd526ea737a4f666c24117b9f16ab232
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2023-10-06 19:19:55 +0530

    flappy

--- ui/MenuLayer.gd
+++ ui/MenuLayer.gd
@@ -2,6 +2,9 @@ extends CanvasLayer
 
 signal start_game
 
+var game_started=false
+
 func _input(event):
-	if event.is_action_pressed("flap"):
-		
+	if event.is_action_pressed("flap") && !game_started:
+		emit_signal("start_game")
+		game_started=true
