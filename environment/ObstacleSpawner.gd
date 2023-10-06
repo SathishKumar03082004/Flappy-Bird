eecec27071ae7d498d72bde5917080d2117f12a4
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2023-10-06 17:07:17 +0530

    flappy

--- environment/ObstacleSpawner.gd
+++ environment/ObstacleSpawner.gd
@@ -3,3 +3,10 @@ extends Node2D
 onready var timer=$Timer
 
 var Obstacle=preload("res://environment/Obstacle.tscn")
+
+
+func _on_Timer_timeout():
+	pass # Replace with function body.
+
+func spawn_obstacle():
+	pass
