386e6606a479fcee6a69dc4c4d5afe34e9b7de16
SathishKumar03082004 <sathishkumar03082004@gmail.com>
2023-10-06 09:28:16 +0530

    flappy

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -8,10 +8,13 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("flap"):
 		if !started:
 			start()
-
+		flap()
 
 func start():
 	if started:return
 	started=true
 	gravity_scale =5.0
 	animator.play("flap")
+
+func flap():
+	pass
